728bf09da0a9ff1ee5ed77e1ef2531bdcffcee0c
MuhtarBey <bektasmert2002@gmail.com>
2025-05-26 20:36:01 +0300

    conflict solve 1

---
+++ Assets/Mert Bektaş/Scripts/Scripts On Trigger/CurrentNpc.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CurrentNpc : MonoBehaviour
+{
+    public GameObject currentNpc { get; private set; }
+public static CurrentNpc Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Npc"))
+        {
+            currentNpc = other.gameObject;
+            //Debug.Log("trigger aktif" + currentNpc);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Npc")&& other.gameObject==currentNpc)
+        {
+            currentNpc =null;
+        }  
+    }
+}
